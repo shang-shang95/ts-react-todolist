4ae3bac256d50f48e99b80d590be26f5c887f4bb
shang-shang95 <qqxn7ra9k@gmail.com>
2023-10-07 05:31:45 +0900

    Add input field and submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+type Todo = {
+  value: string;
+  id: number;
+  checked: boolean;
+};
+
 function App() {
-  return <div className="App"></div>;
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 新しいTodoを宣言
+    const newTodo: Todo = {
+      value: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Todoリスト with Typescript</h2>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            className="inputText"
+          />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
